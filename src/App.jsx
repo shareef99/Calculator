6da8ecd3875c5f1943dfa4f32134dc246b9b401d
shareef <nadeemshareef934@gmail.com>
2021-01-25 22:14:41 +0530

    still have to implement calculator

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
         { id: "subtract", value: "-" },
         { id: "multiply", value: "*" },
         { id: "divide", value: "/" },
+        { id: "decimal", value: "." },
     ];
 
     // useState
@@ -27,23 +28,79 @@ function App() {
 
     //Handle functions
     const handleNumber = (number) => {
-        setDisplay((perState) => [...perState, number]);
+        setDisplay((preState) => [...preState, number]);
+    };
+
+    const handleOperations = (operation) => {
+        setDisplay((preState) => [...preState, operation]);
     };
 
     const handleClear = () => {
-        setDisplay([0]);
+        setDisplay([]);
+    };
+
+    const handleDot = () => {
+        setDisplay((preState) => [...preState, "."]);
+    };
+
+    const twoArrays = (operation) => {
+        const operationLocation = display.indexOf(operations);
+        const arrBeforeOperation = display.slice(0, operationLocation);
+        const arrAfterOperation = display.slice(
+            operationLocation + 1,
+            display.length
+        );
+        const firstStringValue = arrAfterOperation.join("");
+        const secondStringValue = arrBeforeOperation.join("");
+        const firstNumber = parseInt(firstStringValue, 10);
+        const secondNumber = parseInt(secondStringValue, 10);
+        return [firstNumber, secondNumber];
+    };
+
+    const handleCalculations = () => {
+        if (display.includes("/")) {
+            const [firstValue, secondValue] = twoArrays("/");
+            const Result = firstValue / secondValue;
+            setDisplay([Result]);
+        }
+        if (display.includes("*")) {
+            const [firstValue, secondValue] = twoArrays("*");
+            const Result = firstValue * secondValue;
+            setDisplay([Result]);
+        }
+        if (display.includes("-")) {
+            const [firstValue, secondValue] = twoArrays("-");
+            const Result = firstValue - secondValue;
+            setDisplay([Result]);
+        }
+        if (display.includes("+")) {
+            const addLocation = display.indexOf("+");
+            const arrBeforeAdd = display.slice(0, addLocation);
+            const arrAfterAdd = display.slice(addLocation + 1, display.length);
+            const Result =
+                parseFloat(arrBeforeAdd.join(""), 10) +
+                parseFloat(arrAfterAdd.join(""), 10);
+            setDisplay([Result]);
+        }
     };
 
     return (
         <>
             <section className="m-20">
-                <button id="equals">=</button>
+                <button
+                    id="equals"
+                    className="m-2 p-2"
+                    onClick={handleCalculations}
+                >
+                    =
+                </button>
                 {numbers.map((number) => {
                     return (
                         <button
                             id={number.id}
                             key={number.id}
                             onClick={() => handleNumber(number.num)}
+                            className="m-2 p-2"
                         >
                             {number.num}
                         </button>
@@ -51,17 +108,23 @@ function App() {
                 })}
                 {operations.map((operation) => {
                     return (
-                        <button id={operation.id} key={operation.id}>
+                        <button
+                            id={operation.id}
+                            key={operation.id}
+                            onClick={() => {
+                                handleOperations(operation.value);
+                            }}
+                            className="m-2 p-2"
+                        >
                             {operation.value}
                         </button>
                     );
                 })}
-                <button id="decimal">.</button>
-                <button id="clear" onClick={handleClear}>
+                <button id="clear" onClick={handleClear} className="m-2 p-2">
                     AC
                 </button>
                 <div>
-                    <p id="display">
+                    <p id="display" className="m-2 p-2" onClick={handleDot}>
                         {display.length === 0 ? 0 : display.map((num) => num)}
                     </p>
                 </div>
