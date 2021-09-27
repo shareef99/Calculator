f07e19bcf88039cd47bb0f9449d5c9e95d155251
shareef <nadeemshareef934@gmail.com>
2021-09-28 00:04:42 +0530

    🙌 setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    //Constant
+
+    // useState
+
+    // Handle functions
+
+    return <section className="m-20">Calculator</section>;
+}
+
+export default App;
